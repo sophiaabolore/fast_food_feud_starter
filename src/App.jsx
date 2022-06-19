f47ff6aecec5fa96f29e907669679f69cf0e8a9a
Opebiyi Abolore <sophiaabolore@fb.com>
2022-06-19 14:07:34 -0700

    Tried step 1 of the lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
-
+import Header from "./components/Header/Header.jsx"
+import Instructions from "./components/Instructions/Instructions.jsx"
 // don't move this!
 export const appInfo = {
   title: `Fast Food Feud 🍔!`,
@@ -33,15 +34,16 @@ export function App() {
 
       {/* MAIN COLUMN */}
       <div className="container">
-        {/* HEADER GOES HERE */}
+        {/* Header */}
+        <Header header={appInfo}/>
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
           <div className="restaurants options">{/* YOUR CODE HERE */}</div>
         </div>
-
-        {/* INSTRUCTIONS GO HERE */}
+        {/* Instructions  */}
+        <Instructions instructions={appInfo.instructions}/>
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import * as React from "react"
 import "./Header.css"
 
-export function Header(props) {
+export default function Header(props) {
   return (
     <header className="header">
-      <h1 className="title"></h1>
-      <h4 className="tagline"></h4>
-      <p className="description"></p>
+      <h1 className={props.header.title}></h1>
+      <h4 className={props.header.tagline}></h4>
+      <p className={props.header.description}></p>
     </header>
   )
 }
 
-export default Header
+// export default Header(props)
